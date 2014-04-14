351bc8259012e398cb04e55511b3c4218705baaf
Tim Kelly <tim.kelly@myxer.com>
2014-04-14 10:20:46 -0500

    add a test class

---
+++ MyLibraryProject/MyLibrary/src/main/java/com/tk/example/mylibrary/MyLibClass.java
@@ -0,0 +1,12 @@
+package com.tk.example.mylibrary;
+
+/**
+* Created by Tim Kelly on 4/14/14.
+        */
+public class MyLibClass {
+
+    public static String testMethod(){
+        return "You have successfully used this library!";
+    }
+
+}
